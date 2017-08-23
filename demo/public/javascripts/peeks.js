46a51b0454288ff61f734b86d94b9261b9002746
r1 <erwan>
2017-08-23 11:43:43 +0200

    Extended primitives support

--- demo/public/javascripts/peeks.js
+++ demo/public/javascripts/peeks.js
@@ -2,7 +2,9 @@
 		typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 		typeof define === 'function' && define.amd ? define(['exports'], factory) :
 		(factory((global.PEEKS = global.PEEKS || {})));
-}(this, (function (exports) { 'use strict';
+}(this, (function (exports) {
+
+	'use strict';
 
 	function EventDispatcher() {}
 		Object.assign( EventDispatcher.prototype, {
@@ -56,11 +58,9 @@
 	}
 	Object.assign( Node.prototype, EventDispatcher.prototype,
 		{
-			isObject3D: true,
-			onBeforeRender: function () {},
-			onAfterRender: function () {},
-			applyMatrix: function ( matrix ) {
-
+			add: function ( node ) {
+				node.parent = this;
+				this.children[ this.children.length ] = node;
 			},
 		}
 	);
@@ -68,18 +68,71 @@
 	// Asset
 	function Asset( ) {
 		Node.call( this );
+		this.position = [0, 0, 0];
+		this.rotation = [0, 0, 0];
+		this.size = [1, 1, 1];
+		this.color = [.7, .7, .7]; // Default color is grey, not white
+		this.type = 'Asset';
+		this.primitive = Asset.PrimitiveNone;
 	}
+
+	Asset.PrimitiveNone = 0;
+	Asset.PrimitivePlane = 1;
+
 	Asset.prototype = Object.assign(Object.create( Node.prototype ),
 		{
 			constructor: Asset,
-			set: function ( value ) {
+
+			setPosition: function ( x, y, z ) {
+				if (x) this.position[0] = x;
+				if (y) this.position[1] = y;
+				if (z) this.position[2] = z;
 			},
+
+			setRotation: function ( x, y, z ) {
+				if (x) this.rotation[0] = x;
+				if (y) this.rotation[1] = y;
+				if (z) this.rotation[2] = z;
+			},
+
+			setSize: function(width, height, depth) {
+				if (width) 	this.size[0] = width;
+				if (height)	this.size[1] = height;
+				if (depth)	this.size[2] = depth;
+			},
+
+			setColor: function(red, green, blue) {
+				if (red) 		this.color[0] = red;
+				if (green) 	this.color[1] = green;
+				if (blue) 	this.color[2] = blue;
+			},
+		}
+	);
+
+	function Scene( ) {
+		Asset.call( this );
+	}
+	Scene.prototype = Object.assign(Object.create( Asset.prototype ),
+		{
+			constructor: Scene,
+		}
+	);
+
+	function Plane( ) {
+		Asset.call( this );
+		this.primitive = Asset.PrimitivePlane;
+	}
+	Plane.prototype = Object.assign(Object.create( Asset.prototype ),
+		{
+			constructor: Plane,
 		}
 	);
 
 	exports.EventDispatcher = EventDispatcher;
 	exports.Node = Node;
 	exports.Asset = Asset;
+	exports.Scene = Scene;
+	exports.Plane = Plane;
 
 	Object.defineProperty(exports, '__esModule', { value: true });
 })));
--- demo/public/javascripts/peeks_three.js
+++ demo/public/javascripts/peeks_three.js
@@ -1,10 +1,48 @@
 PEEKS.Asset.prototype.threeSynch = function() {
 	if (!this.threeNode) {
-		var geometry = new THREE.PlaneGeometry( 1, 1 );
-		var material = new THREE.MeshBasicMaterial( {color: 0xaaaaaa, side: THREE.DoubleSide} );
-		var plane = new THREE.Mesh( geometry, material );
-		this.threeNode = plane;
+		if (this.primitive) {
+			if (this.primitive == PEEKS.Asset.PrimitivePlane) {
+				var geometry = new THREE.PlaneGeometry( 1, 1 );
+				var material = new THREE.MeshBasicMaterial( {color: 0xaaaaaa, side: THREE.DoubleSide} );
+				var plane = new THREE.Mesh( geometry, material );
+				this.threeNode = plane;
+			} else {
+				this.threeNode = new THREE.Object3D();
+			}
+
+			this.threeNode.position.x = this.position[0];
+			this.threeNode.position.y = this.position[1];
+			this.threeNode.position.z = this.position[2];
+
+			this.threeNode.rotation.x = THREE.Math.degToRad(this.rotation[0]);
+			this.threeNode.rotation.y = THREE.Math.degToRad(this.rotation[1]);
+			this.threeNode.rotation.z = THREE.Math.degToRad(this.rotation[2]);
+
+			this.threeNode.scale.x = this.size[0];
+			this.threeNode.scale.y = this.size[1];
+			this.threeNode.scale.z = this.size[2];
+
+			if (this.threeNode.material) {
+				this.threeNode.material.color.r = this.color[0];
+				this.threeNode.material.color.g = this.color[1];
+				this.threeNode.material.color.b = this.color[2];
+			}
+		} else {
+			this.threeNode = new THREE.Object3D();
+		}
 	}
 
+	for (var childI = 0; childI < this.children.length; childI++) {
+		var child = this.children[childI];
+		if (!child.threeNode) {
+			this.threeNode.add(child.threeSynch());
+		}
+	}
+
+	return this.threeNode;
+}
+
+PEEKS.Asset.prototype.threeGetNode = function() {
+	this.threeSynch();
 	return this.threeNode;
 }
