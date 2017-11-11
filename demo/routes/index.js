490566290525d176789e8fc2970c47f8a39a155d
r1 <erwan>
2017-11-11 18:17:45 +0100

    Allow custom pages url params

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Peeks' });
+router.get('/', function(request, response, next) {
+    response.render('index', { title: 'Peeks' });
 });
 
 module.exports = router;
