84e5b65976d9a2cab41fbc89ca74bde9e28b29a5
r1 <erwan>
2017-11-19 18:29:29 +0100

    Filled up items for all teams

--- demo/public/assets/sportrade.js
+++ demo/public/assets/sportrade.js
@@ -7,6 +7,38 @@ PEEKS.registerPage('sportrade', function() {
     page.setAssetPath('http://52.25.54.6/?url=https://medias.lequipe.fr/logo-football/');
 
     page.addSiteMapItem('WC 2018', { icon: 'https://upload.wikimedia.org/wikipedia/fr/thumb/f/f7/FIFA_World_Cup_2018_Logo.png/200px-FIFA_World_Cup_2018_Logo.png', description: 'Mars 2018'} );
+    page.addSiteMapItem("WC 2018/Brésil", { icon: '626/300'} );
+    page.addSiteMapItem("WC 2018/Argentine", { icon: '642/300'} );
+    page.addSiteMapItem("WC 2018/Uruguay", { icon: '885/300'} );
+    page.addSiteMapItem("WC 2018/Colombie", { icon: '640/300'} );
+    page.addSiteMapItem("WC 2018/Égypte", { icon: '654/300'} );
+    page.addSiteMapItem("WC 2018/Maroc", { icon: '627/300'} );
+    page.addSiteMapItem("WC 2018/Nigéria", { icon: '634/300'} );
+    page.addSiteMapItem("WC 2018/Tunisie", { icon: '639/300'} );
+    page.addSiteMapItem("WC 2018/Sénégal", { icon: '951/300'} );
+    page.addSiteMapItem("WC 2018/Arabie Saoudite", { icon: '631/300'} );
+    page.addSiteMapItem("WC 2018/Iran", { icon: '637/300'} );
+    page.addSiteMapItem("WC 2018/République de Corée", { icon: '636/300'} );
+    page.addSiteMapItem("WC 2018/Japon", { icon: '643/300'} );
+    page.addSiteMapItem("WC 2018/Allemagne", { icon: '159/300'} );
+    page.addSiteMapItem("WC 2018/Angleterre", { icon: '170/300'} );
+    page.addSiteMapItem("WC 2018/Belgique", { icon: '644/300'} );
+    page.addSiteMapItem("WC 2018/Croatie", { icon: '187/300'} );
+    page.addSiteMapItem("WC 2018/Espagne", { icon: '175/300'} );
+    page.addSiteMapItem("WC 2018/France", { icon: '165/300'} );
+    page.addSiteMapItem("WC 2018/Islande", { icon: '167/300'} );
+    page.addSiteMapItem("WC 2018/Pologne", { icon: '173/300'} );
+    page.addSiteMapItem("WC 2018/Portugal", { icon: '181/300'} );
+    page.addSiteMapItem("WC 2018/Russie", { icon: '164/300'} );
+    page.addSiteMapItem("WC 2018/Serbie", { icon: '4256/300'} );
+    page.addSiteMapItem("WC 2018/Suède", { icon: '172/300'} );
+    page.addSiteMapItem("WC 2018/Suisse", { icon: '152/300'} );
+    page.addSiteMapItem("WC 2018/Costa Rica", { icon: '5000000000000000000001351/300'} );
+    page.addSiteMapItem("WC 2018/Mexique", { icon: '635/300'} );
+    page.addSiteMapItem("WC 2018/Panama", { icon: '1207/300'} );
+    page.addSiteMapItem("WC 2018/Pérou", { icon: '650/300'} );
+    page.addSiteMapItem("WC 2018/Danemark", { icon: '151/300'} );
+    page.addSiteMapItem("WC 2018/Australie", { icon: '1889/300'} );
     page.addSiteMapItem("Ligue1 2019", { icon: '/assets/sportrade_icon_france_ligue1.png', description: 'Juin 2018'} );
     page.addSiteMapItem("Ligue1 2019/PSG", { icon: '26/300'} );
     page.addSiteMapItem("Ligue1 2019/OM", { icon: '6/300'} );
@@ -29,6 +61,26 @@ PEEKS.registerPage('sportrade', function() {
     page.addSiteMapItem("Ligue1 2019/OGC Nice", { icon: '46/300' } );
     page.addSiteMapItem("Ligue1 2019/Stade Rennais FC", { icon: '14/300' } );
     page.addSiteMapItem("Ligue1 2020", { icon: '/assets/sportrade_icon_france_ligue1.png', description: 'Juin 2019'} );
+    page.addSiteMapItem("Ligue1 2020/PSG", { icon: '26/300'} );
+    page.addSiteMapItem("Ligue1 2020/OM", { icon: '6/300'} );
+    page.addSiteMapItem("Ligue1 2020/OL", { icon: '22/300' } );
+    page.addSiteMapItem("Ligue1 2020/As Monaco", { icon: '25/300' } );
+    page.addSiteMapItem("Ligue1 2020/FCG Bordeaux", { icon: '18/300' } );
+    page.addSiteMapItem("Ligue1 2020/FC Nantes", { icon: '15/300' } );
+    page.addSiteMapItem("Ligue1 2020/EA Guingamp", { icon: '37/300' } );
+    page.addSiteMapItem("Ligue1 2020/SCO Angers", { icon: '374/300' } );
+    page.addSiteMapItem("Ligue1 2020/Toulouse FC", { icon: '12/300' } );
+    page.addSiteMapItem("Ligue1 2020/Amiens SC", { icon: '44/300' } );
+    page.addSiteMapItem("Ligue1 2020/RC Strasbourg", { icon: '13/300' } );
+    page.addSiteMapItem("Ligue1 2020/Lille OSC", { icon: '43/300' } );
+    page.addSiteMapItem("Ligue1 2020/AS Saint-Etienne", { icon: '38/300' } );
+    page.addSiteMapItem("Ligue1 2020/Dijon FCO", { icon: '202/300' } );
+    page.addSiteMapItem("Ligue1 2020/FC Metz", { icon: '20/300' } );
+    page.addSiteMapItem("Ligue1 2020/SM Caen", { icon: '41/300' } );
+    page.addSiteMapItem("Ligue1 2020/Montpellier HSC", { icon: '17/300' } );
+    page.addSiteMapItem("Ligue1 2020/ESTAC", { icon: '30/300' } );
+    page.addSiteMapItem("Ligue1 2020/OGC Nice", { icon: '46/300' } );
+    page.addSiteMapItem("Ligue1 2020/Stade Rennais FC", { icon: '14/300' } );
 
     page.setSiteMapMenuPath('');
 
@@ -49,24 +101,24 @@ PEEKS.registerPage('sportrade', function() {
         if (screen) {
             screen.destroy();
         }
-        screen = page.addScreen({
-            radius: 4,
-        });
 
         var items = page.querySiteMapMenuAssets();
         if (items) {
             var itemCount = items.length;
+            var itemCountMin = itemCount < 15 ? 15 : itemCount;
+
+            screen = page.addScreen({
+                radius: itemCountMin / 4,
+            });
+
             for (var itemI = 0; itemI < itemCount; itemI++) {
                 var item = items[itemI];
                 var xIndex = itemI;
-                var yOffset = 0;
-                while (xIndex >= 20) {
-                    yOffset += .2;
-                    xIndex -= 9;
-                }
-                var xOffset = (xIndex % 2 === 0) ? (-xIndex / 20) : (xIndex + 1) / 20;
+                var xOffset = (xIndex % 2 === 0) ?
+                    (-xIndex / itemCountMin) :
+                    (xIndex + 1) / itemCountMin;
                 var asset = screen.addAsset({
-                    position: [xOffset, yOffset, 0],
+                    position: [xOffset, 0, 0],
                     size: .4,
                 });
                 asset.addView({
@@ -100,9 +152,19 @@ PEEKS.registerPage('sportrade', function() {
                 }
             }
         }
+        screen.addAsset({
+            position: [0, -.3, 0],
+        }).addText({
+            position: [0, 0, 1],
+            fontSize: 100,
+            size: 1,
+            text: 'RDV en Mars 2018'
+        });
     }
 
     refresh();
+
+    //page.addPage('peeks_toolbar');
 
 	return page;
 });
