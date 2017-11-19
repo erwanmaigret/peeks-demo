14bbc80564aead986e7afffb5b10d18e9d90c1dd
r1 <erwan>
2017-11-19 14:42:36 +0100

    Improved moto rotations

--- demo/public/assets/tron.js
+++ demo/public/assets/tron.js
@@ -31,11 +31,11 @@ PEEKS.registerPage('tron', function() {
         var p1 = this.getScene().computeOffsetFromCamera(offsetFromCamera);
 
         if (moto == undefined) {
-            moto = this.getScene().getCamera().addGeometry({
+            moto = page.addGeometry({
                 geometry: 'assets/tron_moto.obj',
                 size: .02,
                 rotation: [0, 180, 0],
-                position: offsetFromCamera,
+                position: p1,
             });
             moto.trail = {
                 lastPosition: p1.slice(),
@@ -43,8 +43,11 @@ PEEKS.registerPage('tron', function() {
             };
         }
 
+        moto.setPosition(p1);
+
         var p0 = moto.trail.lastPosition;
         var offsetV2 = [p1[0] - p0[0], p1[2] - p0[2]];
+
         var distance = Math.sqrt(offsetV2[0] * offsetV2[0] + offsetV2[1] * offsetV2[1]);
         if (distance > .02) {
             moto.trail.lastPosition = p1.slice();
@@ -52,20 +55,22 @@ PEEKS.registerPage('tron', function() {
             if (moto.trail.ribbons !== undefined) {
                 moto.trail.ribbons.destroy();
             }
-            var position = this.getScene().computeOffsetFromCamera(offsetFromCamera);
             moto.trail.ribbons = page.addAsset();
-            moto.trail.ribbons.addRibbon({
-                position: [0, .2, 0],
-                points: moto.trail.points,
-            });
-            moto.trail.ribbons.addRibbon({
-                position: [0, .1, 0],
-                points: moto.trail.points,
-            });
-            moto.trail.ribbons.addRibbon({
-                position: [0, 0, 0],
-                points: moto.trail.points,
-            });
+            moto.trail.ribbons.addRibbon({position: [0, .2, 0], points: moto.trail.points });
+            moto.trail.ribbons.addRibbon({position: [0, .1, 0], points: moto.trail.points });
+            moto.trail.ribbons.addRibbon({position: [0, 0, 0], points: moto.trail.points });
+        }
+
+        if (moto.trail.points.length >= 2) {
+            var shift = moto.trail.points.length >= 10 ? 10 : moto.trail.points.length;
+            var a = moto.trail.points[moto.trail.points.length - shift];
+            var b = moto.trail.points[moto.trail.points.length - 1];
+            var dir = [b[0] - a[0], b[1] - a[1], b[2] - a[2]];
+            var length = Math.sqrt(dir[0] * dir[0] + dir[1] * dir[1] + dir[2] * dir[2])
+            dir = [dir[0] / length, dir[1] / length, dir[2] / length];
+            var cos = dir[0];
+            var sign =  dir[2] < 0 ? 1 : -1;
+            moto.setRotation([0, 90 + sign * Math.acos(cos) * 180 / Math.PI, 0]);
         }
     };
 
