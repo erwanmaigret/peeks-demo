85ccd9e98df0f26ff9d53d684e434de7c2e62668
erwan <erwanmaigret@gmail.com>
2018-01-04 12:37:23 +0100

    Terraworlds

---
+++ terraworlds/routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(request, response, next) {
+    response.render('index', { title: 'Terraworlds' });
+});
+
+module.exports = router;
