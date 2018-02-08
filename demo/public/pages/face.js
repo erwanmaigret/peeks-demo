c99b1195face13189aa4aceaf90adbf6b5fcbd26
erwanmaigret <erwanmaigret@gmail.com>
2018-02-08 23:54:53 +0100

    Reorganized Face Tracking

--- demo/public/pages/face.js
+++ demo/public/pages/face.js
@@ -1,24 +1,84 @@
-function PeeksTracker(width, height) {
-    if (width !== 0 && height !== 0) {
-        var tracker = {
-            init: PeeksTrackerInit,
-            update: PeeksTrackerUpdate,
-        };
-        tracker.init(width, height);
-        return tracker;
+function PeeksTracker(video, image) {
+    if (image) {
+        var width = image.width;
+        var height = image.height;
+
+        if (width !== 0 && height !== 0) {
+            var tracker = {
+                init: PeeksTrackerInit,
+                update: PeeksTrackerUpdate,
+            };
+            tracker.init(video, image);
+            return tracker;
+        }
     }
 }
 
-
-function PeeksTrackerInit(width, height) {
-    this.videoCapture;
-    this.srcMat;
-    this.grayMat;
-    this.faceClassifier;
-    this.eyeClassifier;
+function PeeksTrackerInit(video, image) {
+    this.videoCapture = new cv.VideoCapture(video);
+    this.srcMat = new cv.Mat(image.height, image.width, cv.CV_8UC4);
+    this.grayMat = new cv.Mat(image.height, image.width, cv.CV_8UC1);
+    this.faceClassifier = new cv.CascadeClassifier();
+    this.faceClassifier.load('haarcascade_frontalface_default.xml');
+    this.eyeClassifier = new cv.CascadeClassifier();
+    this.eyeClassifier.load('haarcascade_eye.xml');
 }
 
-function PeeksTrackerUpdate(image) {
+function PeeksTrackerUpdate(video, image) {
+    this.srcMat.data.set(image.data);
+    cv.cvtColor(this.srcMat, this.grayMat, cv.COLOR_RGBA2GRAY);
+
+    this.faces = [];
+    this.eyes = [];
+    var size;
+    var faceVect = new cv.RectVector();
+    var faceMat = new cv.Mat();
+    var trackEyes = false;
+    if (trackEyes) {
+        cv.pyrDown(this.grayMat, faceMat);
+        size = faceMat.size();
+    } else {
+        cv.pyrDown(this.grayMat, faceMat);
+        if (image.width > 320) {
+            cv.pyrDown(faceMat, faceMat);
+        }
+        size = faceMat.size();
+    }
+    this.faceClassifier.detectMultiScale(faceMat, faceVect);
+
+    imageWidth = faceMat.size().width;
+    imageHeight = faceMat.size().height;
+    var faceCount = faceVect.size() > 1 ? 1 : faceVect.size();
+    for (var i = 0; i < faceCount; i++) {
+        var face = faceVect.get(i);
+        this.faces.push({
+            position: [
+                (face.x + face.width / 2) / imageWidth - .5,
+                -(face.y + face.height / 2) / imageHeight + .5,
+                0],
+            size: [
+                face.width / imageWidth,
+                face.height / imageHeight,
+                1],
+        });
+
+        /*
+        if (trackEyes) {
+            var eyeVect = new cv.RectVector();
+            var eyeMat = faceMat.roi(face);
+            eyeClassifier.detectMultiScale(eyeMat, eyeVect);
+            for (var i = 0; i < eyeVect.size(); i++) {
+                var eye = eyeVect.get(i);
+                this.eyes.push(new cv.Rect(face.x + eye.x, face.y + eye.y, eye.width, eye.height));
+            }
+            eyeMat.delete();
+            eyeVect.delete();
+        }
+        */
+    }
+    faceMat.delete();
+    faceVect.delete();
+
     return true;
 }
 
@@ -28,18 +88,18 @@ PEEKS.registerPage('Face', function(scene) {
         gyroscope: 'off',
     });
 
-    page.initTracker = function(imageData) {
+    page.initTracker = function(video, imageData) {
         if (imageData && !page.tracker) {
-            page.tracker = PeeksTracker();
+            page.tracker = PeeksTracker(video, imageData);
         }
 
         return page.tracker !== undefined;
     };
 
-    page.updateTracker = function(imageData) {
-        this.initTracker(imageData);
+    page.updateTracker = function(video, imageData) {
+        this.initTracker(video, imageData);
         if (this.tracker) {
-            return this.tracker.update(imageData);
+            return this.tracker.update(video, imageData);
         }
     };
 
@@ -61,83 +121,20 @@ PEEKS.registerPage('Face', function(scene) {
             var video = scene.DOMarGetElement();
             if (video) {
                 PEEKS.addExtensionListener('cv', function(cv) {
-                    var videoCapture;
-                    var srcMat;
-                    var grayMat;
-                    var faceClassifier;
-                    var eyeClassifier;
                     page.onUpdate = function() {
                         if (tracking) {
                             var imageData = scene.getArImageData();
-                            if (this.updateTracker(imageData)) {
-                                var videoWidth = imageData.width;
-                                var videoHeight = imageData.height;
-                                if (videoCapture === undefined) {
-                                    videoCapture = new cv.VideoCapture(video);
-                                    srcMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
-                                    grayMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC1);
-                                    faceClassifier = new cv.CascadeClassifier();
-                                    faceClassifier.load('haarcascade_frontalface_default.xml');
-                                    eyeClassifier = new cv.CascadeClassifier();
-                                    eyeClassifier.load('haarcascade_eye.xml');
-                                }
-
-                                srcMat.data.set(imageData.data);
-                                cv.cvtColor(srcMat, grayMat, cv.COLOR_RGBA2GRAY);
-
-                                var faces = [];
-                                var eyes = [];
-                                var size;
-                                var faceVect = new cv.RectVector();
-                                var faceMat = new cv.Mat();
-                                var trackEyes = false;
-                                if (trackEyes) {
-                                    cv.pyrDown(grayMat, faceMat);
-                                    size = faceMat.size();
-                                } else {
-                                    cv.pyrDown(grayMat, faceMat);
-                                    if (videoWidth > 320) {
-                                        cv.pyrDown(faceMat, faceMat);
-                                    }
-                                    size = faceMat.size();
-                                }
-                                faceClassifier.detectMultiScale(faceMat, faceVect);
-
-                                var faceCount = faceVect.size() > 1 ? 1 : faceVect.size();
+                            if (this.updateTracker(video, imageData)) {
+                                var faceCount = this.tracker.faces.length;
+                                faceCount = faceCount > 1 ? 1 : faceCount;
                                 for (var i = 0; i < faceCount; i++) {
-                                    var face = faceVect.get(i);
-                                    faces.push(new cv.Rect(face.x, face.y, face.width, face.height));
-
                                     if (!arView.faceMask) {
                                         arView.faceMask = arView.addRing();
                                     }
 
-                                    imageWidth = faceMat.size().width;
-                                    imageHeight = faceMat.size().height;
-                                    var position = [ (face.x + face.width / 2) / imageWidth - .5,
-                                                    -(face.y + face.height / 2) / imageHeight + .5,
-                                                    0];
-                                    var size = [face.width / imageWidth,
-                                                face.height / imageHeight,
-                                                1];
-
-                                    arView.faceMask.setPosition(position);
-                                    arView.faceMask.setSize(size);
-
-                                    if (trackEyes) {
-                                        var eyeVect = new cv.RectVector();
-                                        var eyeMat = faceMat.roi(face);
-                                        eyeClassifier.detectMultiScale(eyeMat, eyeVect);
-                                        for (var i = 0; i < eyeVect.size(); i++) {
-                                            var eye = eyeVect.get(i);
-                                            eyes.push(new cv.Rect(face.x + eye.x, face.y + eye.y, eye.width, eye.height));
-                                        }
-                                        eyeMat.delete();
-                                        eyeVect.delete();
-                                    }
+                                    arView.faceMask.setPosition(this.tracker.faces[i].position);
+                                    arView.faceMask.setSize(this.tracker.faces[i].size);
                                 }
-                                faceMat.delete();
-                                faceVect.delete();
                             }
                         }
                     };
