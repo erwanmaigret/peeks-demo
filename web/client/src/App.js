2751ba62c5c6c49fbf12273a0334ded8f0e33b34
erwanmaigret <erwanmaigret@gmail.com>
2018-02-13 17:09:30 +0100

    WIP

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -22,14 +22,41 @@ class MenuBar extends React.Component {
         <div>
           <div className="menuBar">
             {this.renderMenuItem('PRODUCT')}
-            {this.renderMenuItem('PRICING')}
-            {this.renderMenuItem('API')}
             {this.renderMenuItem('TEAM')}
-            {this.renderMenuItem('CONTACT')}
           </div>
         </div>
       );
     }
+}
+
+class RequestTrialForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Thanks for your interest in Peeks!');
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Request Trial" />
+      </form>
+    );
+  }
 }
 
 class App extends Component {
@@ -47,10 +74,27 @@ class App extends Component {
                     <td align="right"><MenuBar/></td>
                     </tr></tbody></table></div>
                 </header>
-                <p className="intro">
-                More coming soon.
-                </p>
+                <div><table width='100%'><tbody><tr>
+                <td>
+                <div className="h1">We make VR a reality.</div>
+                <p>Immersive web and mobile VR/AR technology now made available to the retail industry.</p>
+                </td>
+                </tr></tbody></table></div>
+                <div>
+                <div className="h1">Meet our team.</div>
+                <table width='100%'><tbody><tr>
+                <td>
+                <p>Ryan</p>
+                </td>
+                <td>
+                <p>Erwan</p>
+                </td>
+                </tr></tbody></table></div>
                 <footer className="footer">
+                <h1>We bring your inventory to life</h1>
+                <p>Our VR technology is ultra easy to implement. Try it!</p>
+                <RequestTrialForm/>
+                <p className="copyright">&copy; All rights reserved - Peeks Technologies 2018</p>
                 </footer>
             </div>
         );
