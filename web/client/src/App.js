b584b98f6d007d84f097ba6404c3ad29fd6aab25
erwanmaigret <erwanmaigret@gmail.com>
2018-02-13 16:18:57 +0100

    WIP layout

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -2,20 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class MenuItem extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Peeks</h1>
-        </header>
-        <p className="App-intro">
-          More coming soon.
-        </p>
-      </div>
+      <button className="menuItem">
+        {this.props.title}
+      </button>
     );
   }
+}
+
+class MenuBar extends React.Component {
+    renderMenuItem(title) {
+      return <MenuItem title={title}/>;
+    }
+
+    render() {
+      return (
+        <div>
+          <div className="menuBar">
+            {this.renderMenuItem('PRODUCT')}
+            {this.renderMenuItem('PRICING')}
+            {this.renderMenuItem('API')}
+            {this.renderMenuItem('TEAM')}
+            {this.renderMenuItem('CONTACT')}
+          </div>
+        </div>
+      );
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="header">
+                    <div><table width='100%'><tbody><tr>
+                    <td align="left">
+                        <table><tbody><tr>
+                        <td><img src={logo} className="logo" alt="logo"/></td>
+                        <td><div className="title"> Peeks</div></td>
+                        </tr></tbody></table>
+                    </td>
+                    <td align="right"><MenuBar/></td>
+                    </tr></tbody></table></div>
+                </header>
+                <p className="intro">
+                More coming soon.
+                </p>
+                <footer className="footer">
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
