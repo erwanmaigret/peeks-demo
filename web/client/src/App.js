98069449a564092169c86667c344f0ec96924ce3
erwanmaigret <erwanmaigret@gmail.com>
2018-02-13 19:18:38 +0100

    WIP

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -63,17 +63,15 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="header">
-                    <div><table width='100%'><tbody><tr>
-                    <td align="left">
-                        <table><tbody><tr>
-                        <td><img src={logo} className="logo" alt="logo"/></td>
-                        <td><div className="title"> Peeks</div></td>
-                        </tr></tbody></table>
-                    </td>
-                    <td align="right"><MenuBar/></td>
-                    </tr></tbody></table></div>
-                </header>
+                <div><table width='100%'><tbody><tr>
+                <td align="left">
+                    <table><tbody><tr>
+                    <td><img src={logo} className="logo" alt="logo"/></td>
+                    <td><div className="title"> Peeks</div></td>
+                    </tr></tbody></table>
+                </td>
+                <td align="right"><MenuBar/></td>
+                </tr></tbody></table></div>
                 <div><table width='100%'><tbody><tr>
                 <td>
                 <div className="h1">We make VR a reality.</div>
@@ -81,13 +79,27 @@ class App extends Component {
                 </td>
                 </tr></tbody></table></div>
                 <div>
-                <div className="h1">Meet our team.</div>
+                <div className="h1">Meet our team!</div>
                 <table width='100%'><tbody><tr>
-                <td>
-                <p>Ryan</p>
+                <td align='center'>
+                <div className="card">
+                <img src="/team-ryan.png" alt="ryan"/>
+                <p className="textGray">CEO</p>
+                <p className="textHighlight">Ryan</p>
+                <p></p>
+                <p className="textSmall">Seasoned hyper growth entrepreneur. Expert in building products, commercializing products and IP, and business development. Executive leader responsible for AR platforms at Blippar. Early strategy leader (first 250 employees) at Uber. MBA from Johns Hopkins University.</p>
+                <img src="/twitter.png" alt="twitter" width='30'/>
+                </div>
                 </td>
-                <td>
-                <p>Erwan</p>
+                <td align='center'>
+                <div className="card">
+                <img src="/team-erwan.png" alt="erwan"/>
+                <p className="textGray">CTO</p>
+                <p className="textHighlight">Erwan</p>
+                <p></p>
+                <p className="textSmall">20+ years experience in 3D software/full stack and engineering leadership. Extensive early stage startup experience (Blippar, Toytalk, Loom.ai). Former Dreamworks Animation R&D engineering lead for feature films (Shrek, Madagascar) and the internal tools used for moviemaking.</p>
+                <img src="/twitter.png" alt="twitter" width='30'/>
+                </div>
                 </td>
                 </tr></tbody></table></div>
                 <footer className="footer">
