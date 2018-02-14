06d110db42fa31675c8cb3eb599e71262e6ba276
erwanmaigret <erwanmaigret@gmail.com>
2018-02-14 19:39:24 +0100

    Initial design update

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -18,14 +18,19 @@ class MenuBar extends React.Component {
     }
 
     render() {
-      return (
-        <div>
-          <div className="menuBar">
-            {this.renderMenuItem('PRODUCT')}
-            {this.renderMenuItem('TEAM')}
-          </div>
-        </div>
-      );
+      let showMenu = false;
+      if (showMenu) {
+          return (
+            <div>
+              <div className="menuBar">
+                {this.renderMenuItem('PRODUCT')}
+                {this.renderMenuItem('TEAM')}
+              </div>
+            </div>
+          );
+      } else {
+          return (<div/>);
+      }
     }
 }
 
@@ -53,8 +58,18 @@ class RequestTrialForm extends React.Component {
         <label>
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
-        <input type="submit" value="Request Trial" />
+        <RoundButton title="Request Trial" />
       </form>
+    );
+  }
+}
+
+class RoundButton extends React.Component {
+  render() {
+    return (
+      <button className="menuItem">
+        {this.props.title}
+      </button>
     );
   }
 }
@@ -65,38 +80,45 @@ class App extends Component {
             <div className="App">
                 <div><table width='100%'><tbody><tr>
                 <td align="left">
-                    <table><tbody><tr>
+                    <table><tbody>
+                    <tr height="10px"></tr>
+                    <tr>
+                    <td width="10px"></td>
                     <td><img src={logo} className="logo" alt="logo"/></td>
                     <td><div className="title"> Peeks</div></td>
-                    </tr></tbody></table>
+                    </tr>
+                    </tbody></table>
                 </td>
                 <td align="right"><MenuBar/></td>
                 </tr></tbody></table></div>
                 <div><table width='100%'><tbody><tr>
-                <td width='40%' valign='top'>
-                <div className="textTitle" align="left"><br/><br/><br/><br/><br/>We make VR a reality.</div>
+                <td width='10%'></td>
+                <td width='40%'>
+                <div className="textTitle" align="left">We make AR and VR a reality.</div>
                 <div className="textDescription" align="left">Immersive web and mobile VR/AR technology now made available to the retail industry.</div>
-                <div className="textDescription" align="left">Customers have fun purchasing online using our AR tool. It brings the fitting room into their hands.</div>
+                <div className="textDescription" align="left">Turn all your inventory into AR. Customers have fun purchasing online using our AR tool. It brings the fitting room into their hands.</div>
                 </td>
-                <td><img src="snapshot-product-1.png" alt="product1" width='80%'/></td>
+                <td><img src="snapshot-product-1.png" alt="product1" width='100%'/></td>
                 </tr></tbody></table></div>
                 <div><table width='100%'><tbody><tr>
                 <td><img src="snapshot-product-2.png" alt="product2" width='100%'/></td>
                 <td width='40%'>
-                <div className="textTitle" align="left"><br/>Web-browser friendly.</div>
+                <div className="textTitle" align="left">Web-browser friendly.</div>
                 <div className="textDescription" align="left">Wether your store is accessible from the web or a native application, our technology brings VR to all software environments.</div>
                 </td>
+                <td width='10%'></td>
                 </tr></tbody></table></div>
                 <div><table width='100%'><tbody><tr>
+                <td width='10%'></td>
                 <td width='40%'>
                 <div className="textTitle" align="left"><br/>Headset compatible.</div>
                 <div className="textDescription" align="left">If you have a VR Headset, you can make it even more immersive.</div>
                 </td>
-                <td><img src="snapshot-product-3.png" alt="product3" width='80%'/></td>
+                <td><img src="snapshot-product-3.png" alt="product3" width='100%'/></td>
                 </tr></tbody></table></div>
                 <div>
-                <div className="h1"><br/><br/><br/>Meet our team!<br/><br/></div>
-                <table width='100%'><tbody><tr>
+                <div className="textTitle"><br/><br/><br/>Meet our team!<br/><br/></div>
+                <table align="center"><tbody><tr>
                 <td align='center'>
                 <div className="card">
                 <img src="/team-ryan.png" alt="ryan"/>
@@ -104,7 +126,7 @@ class App extends Component {
                 <p className="textHighlight">Ryan</p>
                 <p></p>
                 <p className="textSmall">Seasoned hyper growth entrepreneur. Expert in building products, commercializing products and IP, and business development. Executive leader responsible for AR platforms at Blippar. Early strategy leader (first 250 employees) at Uber. MBA from Johns Hopkins University.</p>
-                <img src="/twitter.png" alt="twitter" width='30'/>
+                <a href="https://www.linkedin.com/in/ryanschmaltz/"><img src="/linkedin.png" alt="linkedin" width='30'/></a>
                 </div>
                 </td>
                 <td align='center'>
@@ -114,18 +136,19 @@ class App extends Component {
                 <p className="textHighlight">Erwan</p>
                 <p></p>
                 <p className="textSmall">20+ years experience in 3D software/full stack and engineering leadership. Extensive early stage startup experience (Blippar, Toytalk, Loom.ai). Former Dreamworks Animation R&D engineering lead for feature films (Shrek, Madagascar) and the internal tools used for moviemaking.</p>
-                <img src="/twitter.png" alt="twitter" width='30'/>
+                <a href="https://www.linkedin.com/in/erwanmaigret/"><img src="/linkedin.png" alt="linkedin" width='30'/></a>
                 </div>
                 </td>
                 </tr></tbody></table></div>
                 <footer className="footer">
                 <h1>We bring your inventory to life</h1>
                 <p>Our VR technology is ultra easy to implement. Try it!</p>
-                <RequestTrialForm/>
+                <a href="mailto:info@peeks.io"><p>Request Trial</p></a>
                 <p className="copyright">&copy; All rights reserved - Peeks Technologies 2018</p>
                 </footer>
             </div>
         );
+        // <RequestTrialForm/>
     }
 }
 
