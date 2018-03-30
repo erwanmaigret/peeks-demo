2e9ebc7efc645994cee6d8a9ff34a7db964cea5d
Erwan Maigret <erwanmaigret@gmail.com>
2018-03-30 19:05:52 +0200

    Redirect path on resources

--- demo/public/peeks.cv.js
+++ demo/public/peeks.cv.js
@@ -1,9 +1,9 @@
 var Module = {
-    wasmBinaryFile: '/js/cv/wasm/opencv_js.wasm',
+    wasmBinaryFile: 'https://dev.peeks.io/js/cv/wasm/opencv_js.wasm',
     preRun: [function() {
-        Module.FS_createPreloadedFile('/', 'haarcascade_eye.xml', '/js/cv/data/haarcascade_eye.xml', true, false);
-        Module.FS_createPreloadedFile('/', 'haarcascade_frontalface_default.xml', '/js/cv/data/haarcascade_frontalface_default.xml', true, false);
-        Module.FS_createPreloadedFile('/', 'haarcascade_profileface.xml', '/js/cv/data/haarcascade_profileface.xml', true, false);
+        Module.FS_createPreloadedFile('/', 'haarcascade_eye.xml', 'https://dev.peeks.io/js/cv/data/haarcascade_eye.xml', true, false);
+        Module.FS_createPreloadedFile('/', 'haarcascade_frontalface_default.xml', 'https://dev.peeks.io/js/cv/data/haarcascade_frontalface_default.xml', true, false);
+        Module.FS_createPreloadedFile('/', 'haarcascade_profileface.xml', 'https://dev.peeks.io/js/cv/data/haarcascade_profileface.xml', true, false);
     }],
     _main: function() {
         PEEKS.registerExtension('cv', cv);
